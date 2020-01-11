b86437e905cab95ee074a59d71a95cad90bd1d38
Serafima <2002ssn@gmail.com>
2020-01-11 13:02:03 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-app-bar
+        color="red lighten-1"
+        dark
+      >
+      <v-toolbar-title>Служба доставки подарков</v-toolbar-title>
+      </v-app-bar>
+      
+     <info class ="mt-10"></info>
+    </v-content>
+</v-app>
+</template>
+
+<script>
+import info from './components/info.vue'
+
+
+export default {
+  name: 'App',
+
+  components: {
+    info,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/info.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      color="white"
+      width="200px"
+    >
+        <v-card-text>
+        <p class="font-weight-regular">Доставлено лично Дедом Морозом: {{ santacount }} </p>
+        <p class="font-weight-regular">Доставлено эльфами-курьерами: {{ elfscount }}</p>
+        <p class="font-weight-regular">Доставлено "Почтой России": {{ pochtacount }} </p>
+        </v-card-text>
+    </v-card>
+    <div class = "mt-10">
+      <p class = "display-1 text-center">Заказать доставку</p>
+        <v-col cols="12" sm="4">
+        <div class="my-2">
+          <v-btn color="error" dark large @click="santacount += 1">Доставит дед Мороз</v-btn>
+        </div>
+      <div class="my-2">
+          <v-btn color="error" dark large @click="elfscount += 1">Доставит эльф</v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn color="error" dark large @click="pochtacount += 1">Доставит ПР</v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn color="error" dark large @click="pochtacount += 1, santacount += 1, elfscount += 1">Доставка всеми тремя способами</v-btn>
+        </div>
+        <div class="my-2">
+          <v-btn color="error" dark large @click="persist">Сохранить значения на сервер</v-btn>
+        </div>
+      </v-col>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "clicker",
+    
+    data: () => ({
+        santacount: 0,
+        elfscount: 0,
+        pochtacount: 0
+    }),
+
+     mounted() {
+      if (localStorage.santacount) {
+        this.santacount = localStorage.santacount;
+      }
+      if (localStorage.elfscount) {
+        this.elfscount = localStorage.elfscount;
+      }
+      if (localStorage.pochtacount) {
+        this.pochtacount = localStorage.pochtacount;
+      }
+    },
+
+    methods: {
+       persist() {
+        localStorage.santacount = this.santacount;
+        localStorage.elfscount = this.elfscount;
+        localStorage.pochtacount = this.pochtacount
+      }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
